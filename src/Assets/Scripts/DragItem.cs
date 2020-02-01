97bb5b321e9d9bd11e45991f3b382e9df2878ed8
Matheus Caetano <matheus.caetano@tapps.com.br>
2020-02-01 14:38:59 -0300

    subindo drag level

--- src/Assets/Scripts/DragItem.cs
+++ src/Assets/Scripts/DragItem.cs
@@ -5,22 +5,23 @@ using UnityEngine;
 public class DragItem : MonoBehaviour
 {
     private Collider2D otherCollider2d;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool dragItemComplete = false;
     
     void OnTriggerEnter2D(Collider2D other)
     {
         otherCollider2d = other;
+        Debug.Log("Passando pelo " + otherCollider2d.name);
     }
-
     void OnTriggerExit2D(Collider2D other)
     {
         otherCollider2d = null;
+        Debug.Log("Saindo do collider");
     }
 
+    public bool GetDragItemComplete()
+    {
+        return dragItemComplete;
+    }
     public void EndTouchDrag()
     {
         if(otherCollider2d)
@@ -28,14 +29,9 @@ public class DragItem : MonoBehaviour
             if(gameObject.tag == otherCollider2d.tag)
             {
                 gameObject.transform.position = otherCollider2d.transform.position;
-                GetComponent<Collider2D>().enabled = false;
-                otherCollider2d.enabled = false;
+                dragItemComplete = true;
+                otherCollider2d.GetComponent<GoalItem>().completeGoal();
             }
         }
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
