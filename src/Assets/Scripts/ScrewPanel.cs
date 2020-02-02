9b85700a4e96359bfdf0b04fb7a6cf1497819b1d
Matheus Caetano <matheus.caetano@tapps.com.br>
2020-02-02 11:21:27 -0300

    final quase

--- src/Assets/Scripts/ScrewPanel.cs
+++ src/Assets/Scripts/ScrewPanel.cs
@@ -10,11 +10,19 @@ public class ScrewPanel : MonoBehaviour
 
     private bool allScrewsFinished = false;
 
+    private AudioSource audioSource;
+
     void FixedUpdate()
     {
+        int countTrue = 0;
         for(int i = 0; i < screwGoals.Count; i++)
         {
-            allScrewsFinished = !screwGoals[i].isScrewed;
+            if(!screwGoals[i].isScrewed)
+            countTrue++;
+            if(countTrue >= screwGoals.Count)
+            {
+                allScrewsFinished = true;
+            } 
         }
         if(allScrewsFinished)
         {
@@ -25,11 +33,17 @@ public class ScrewPanel : MonoBehaviour
     void CompleteScrewPanel()
     {
         isPanelComplete = true;
+        audioSource.PlayOneShot(audioSource.clip);
         gameObject.SetActive(false);
     }
 
     public bool GetIsPanelComplete()
     {
         return isPanelComplete;
+    }
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
     }
 }
